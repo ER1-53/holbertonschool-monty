b800f24d3ccbe5dcb4295680921a0c9c641f51dd
letihab <letihabib1822@gmail.com>
2023-09-07 15:12:50 +0200

    solve betty issue

--- monty.c
+++ monty.c
@@ -2,48 +2,48 @@
 
 /**
  * instruction - Function to process Monty instructions from a file.
+ *@file_descriptor: file to read
  */
 void instruction(FILE *file_descriptor)
 {
-    char *line = NULL, *tokenize = NULL;
-    size_t len = 0;
-    ssize_t read;
-    stack_t *stack_st = NULL;
-    unsigned int count = 0;
-    int sniffer = 0;
+	char *line = NULL, *tokenize = NULL;
+	size_t len = 0;
+	ssize_t read;
+	stack_t *stack_st = NULL;
+	unsigned int count = 0;
+	int sniffer = 0;
 
-	 instruction_t instructions[] = {
-        {"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
-        {"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}
-    };
+	instruction_t instructions[] = {
+		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
+		{"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}
+	};
 
-    while ((read = getline(&line, &len, file_descriptor)) != -1)
-    {
-        tokenize = strtok(line, " \n");
-        count++;
-        sniffer = 0;
+	while ((read = getline(&line, &len, file_descriptor)) != -1)
+	{
+		tokenize = strtok(line, " \n");
+		count++;
+		sniffer = 0;
 
-        if (tokenize != NULL)
-        {
-            for (int i = 0; instructions[i].opcode != NULL; i++)
-            {
-                if (strcmp(instructions[i].opcode, tokenize) == 0)
-                {
-                    sniffer = 1;
-                    instructions[i].f(&stack_st, count);
-                    break;
-                }
-            }
-            if (sniffer != 1)
-            {
-                fprintf(stderr, "L%d: unknown instruction %s\n", count, tokenize);
-                monty_free(line, file_descriptor, stack_st);
-				 exit(EXIT_FAILURE);
-            }
-        }
-    }
-
-    monty_free(line, file_descriptor, stack_st);
+	if (tokenize != NULL)
+	{
+		for (int i = 0; instructions[i].opcode != NULL; i++)
+		{
+			if (strcmp(instructions[i].opcode, tokenize) == 0)
+			{
+				sniffer = 1;
+				instructions[i].f(&stack_st, count);
+				break;
+			}
+		}
+		if (sniffer != 1)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", count, tokenize);
+			monty_free(line, file_descriptor, stack_st);
+			exit(EXIT_FAILURE);
+		}
+		}
+	}
+	monty_free(line, file_descriptor, stack_st);
 }
 
 /**
@@ -61,16 +61,15 @@ int main(int argc, char *argv[])
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
-	} 
+	}
 	 FILE *file_descriptor = fopen(argv[1], "r");
-    if (file_descriptor == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (file_descriptor == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    instruction(file_descriptor);
+	instruction(file_descriptor);
 
-    return (0);
+	return (0);
 }
-	
