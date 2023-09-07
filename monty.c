b3b0e932fc04b4703bcb4f65ab63dcaa4b6a922d
letihab <letihabib1822@gmail.com>
2023-09-07 14:51:44 +0200

    update file

--- monty.c
+++ monty.c
@@ -1,6 +1,52 @@
 #include "monty.h"
 
 /**
+ * instruction - Function to process Monty instructions from a file.
+ */
+void instruction(FILE *file_descriptor)
+{
+    char *line = NULL, *tokenize = NULL;
+    size_t len = 0;
+    ssize_t read;
+    stack_t *stack_st = NULL;
+    unsigned int count = 0;
+    int sniffer = 0;
+
+	 instruction_t instructions[] = {
+        {"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
+        {"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}
+    };
+
+    while ((read = getline(&line, &len, file_descriptor)) != -1)
+    {
+        tokenize = strtok(line, " \n");
+        count++;
+        sniffer = 0;
+
+        if (tokenize != NULL)
+        {
+            for (int i = 0; instructions[i].opcode != NULL; i++)
+            {
+                if (strcmp(instructions[i].opcode, tokenize) == 0)
+                {
+                    sniffer = 1;
+                    instructions[i].f(&stack_st, count);
+                    break;
+                }
+            }
+            if (sniffer != 1)
+            {
+                fprintf(stderr, "L%d: unknown instruction %s\n", count, tokenize);
+                monty_free(line, file_descriptor, stack_st);
+				 exit(EXIT_FAILURE);
+            }
+        }
+    }
+
+    monty_free(line, file_descriptor, stack_st);
+}
+
+/**
  * main - Entry point for the Monty interpreter.
  *
  * @argc: Number of command-line arguments.
@@ -11,56 +57,20 @@
 
 int main(int argc, char *argv[])
 {
-	FILE *file_descriptor;
-	char *line = NULL, *tokenize = NULL;
-	size_t len = 0;
-	stack_t *stack_st = NULL;
-	ssize_t read;
-	unsigned int count = 0;
-	int i = 0, sniffer = 0;
-
-	instruction_t instructions[] = {
-		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
-		{"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}
-		};
-
-	if (argc != 2)/* on verfie si il y a 2 arg ./prog fils.m */
+	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
-	} /* on ouvre le fichier en index argv[1] et le lit */
-	file_descriptor = fopen(argv[1], "r");
-	if (file_descriptor == NULL)
-	{/* si il ne s'ouvre pas ou ne contient rien */
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while ((read = getline(&line, &len, file_descriptor)) != -1)
-	{/* line = le contenue; file_des = le stream du fichier; len = nbCaractere*/
-		tokenize = strtok(line, " \n");
-		count++;
-		sniffer = 0;
+	} 
+	 FILE *file_descriptor = fopen(argv[1], "r");
+    if (file_descriptor == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
-		if (tokenize != NULL)
-		{
-			for (i = 0; instructions[i].opcode != NULL; i++)
-			{
-				if (strcmp(instructions[i].opcode, tokenize) == 0)
-				{
-					sniffer = 1;
-					instructions[i].f(&stack_st, count);
-					break;
-				}
-			}
-			if (sniffer != 1)
-			{
-				fprintf(stderr, "L%d: unknown instruction %s\n", count, tokenize);
-				monty_free(line, file_descriptor, stack_st);
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
-	monty_free(line, file_descriptor, stack_st);
-	return (0);
+    instruction(file_descriptor);
+
+    return (0);
 }
-
+	
